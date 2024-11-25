98d3899864cf63c614c80f4532d401a61c1b0d44
SaidGrande <said.hernandez5155@alumnos.udg.mx>
2024-11-24 22:56:39 -0600

    Final final definitivo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,25 @@ import { Box, Typography } from '@mui/material';
 
 const Footer = () => (
   <Box mt="80px" bgcolor="#FFF3F4">
-  <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pt="20px">
-    Made by Said Grande
-  </Typography>
-  <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="20px" textAlign="center" pb="20px">
-  Ten words <b>&quot;aesthetics&quot;</b>
-  </Typography>
-</Box>
-
+    <Typography
+      variant="h5"
+      sx={{ fontSize: { lg: '28px', xs: '20px' } }}
+      mt="41px"
+      textAlign="center"
+      pt="20px"
+    >
+      Made by Said Grande
+    </Typography>
+    <Typography
+      variant="h5"
+      sx={{ fontSize: { lg: '28px', xs: '20px' } }}
+      mt="20px"
+      textAlign="center"
+      pb="20px"
+    >
+      Ten words <b>&quot;aesthetics&quot;</b>
+    </Typography>
+  </Box>
 );
 
 export default Footer;
