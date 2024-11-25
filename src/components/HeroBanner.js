5f22f9ea98eed19cee43a1ecb090f2643257e718
SaidGrande <said.hernandez5155@alumnos.udg.mx>
2024-11-24 21:56:35 -0600

    Final v.1

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -5,7 +5,7 @@ import HeroBannerImage from '../assets/images/PapiZeninD.png';
 
 const HeroBanner = () => (
   <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
-    <Typography color="#d40404" fontWeight="600" fontSize="26px">Be the strongest...s</Typography>
+    <Typography color="#d40404" fontWeight="600" fontSize="26px">Be the strongest...</Typography>
     <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
       Wake Up, Do it <br />
       And Repeat
